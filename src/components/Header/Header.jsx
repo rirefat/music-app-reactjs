fd4565758c1d56e63f27d0b39496616174760636
rirefat <rirefat.official@gmail.com>
2023-01-04 21:53:55 +0600

    done navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,14 @@ import logo from '../../img/MuzicLogo.png';
 import CenterMenu from '../CenterMenu/CenterMenu';
 
 const Header = () => {
-    const buttonStyle ="border-[2px] rounded-[10px] border-[#232A4E] px-[25px] py-[7px]"
+    const buttonStyle ="border-[2px] rounded-[10px] border-[#232A4E] px-[25px] py-[7px] hover:bg-[#232A4E]"
     return (
         <div className="header bg-[#081730] flex items-center justify-between px-[5rem] pt-[2.4rem] text-[1rem]">
             <img src={logo} alt="logo" className='logo w-[42px] h-[42px]'/>
             <CenterMenu></CenterMenu>
 
             <div className="buttons flex">
-                <button className={buttonStyle}>Sign up</button>
+                <button className={`mr-[25px] ` + buttonStyle}>Sign up</button>
                 <button className={buttonStyle}>Log in</button>
             </div>
         </div>
