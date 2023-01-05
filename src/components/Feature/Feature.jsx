4a91340329ffeb337c6e6928058a3c83c4707958
rirefat <rirefat.official@gmail.com>
2023-01-06 02:33:08 +0600

    update features

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -3,12 +3,19 @@ import React from 'react';
 const Feature = (props) => {
     const {icon, title}=props;
     return (
-        <div className='feature'>
+        <div className='feature flex items-center justify-center flex-col relative text-center mx-12'>
             {/* Icon */}
-            <div className="icon">
+            <div className="icon bg-[#081730] rounded-2xl p-5">
                 <img src={require(`../../img/${icon}.png`)} alt="" className='w-[3rem]'/>
             </div>
             <span className="mt-5">{title}</span>
+            <span className="text-[#707070] mt-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, porro minus? ncidunt fugit pariatur eos labore omnis.
+            </span>
+
+            <span className="text-[#e600ff] underline mt-[3rem] hover:cursor-pointer">
+                Learn More
+            </span>
         </div>
     );
 };
