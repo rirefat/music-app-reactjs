526335661c378c5d3b2aa6d31169bf6dc0479786
rirefat <rirefat.official@gmail.com>
2023-03-13 23:31:53 +0600

    update readme file

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div className='search'>
+            {/*================== Left Section ==================*/}
+            <div className="left"></div>
+
+            {/*================= Right Section ==================*/}
+            <div className="right"></div>
+        </div>
+    );
+};
+
+export default Search;
